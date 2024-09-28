64785cd41d509481a239177aeeba60c607c0d1cd
rickie <rcya996001@gmail.com>
2024-09-28 11:35:21 -0400

    added a server.js file

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+
+app = express();
+app.use(cors());
+
+
+
+
+
+app.listen(PORT);
 
